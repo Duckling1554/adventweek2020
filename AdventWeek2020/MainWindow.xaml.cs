8d3210a2f0514948cdeeebc8a757b880220c7241
Любовь Левченко <lllevchenko@edu.hse.ru>
2019-12-19 19:20:01 +0300

    Добавьте файлы проекта.

---
+++ AdventWeek2020/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdventWeek2020
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OneD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void TwoD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ThreeD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void FourD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void FiveD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void SixD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void SevenD_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
