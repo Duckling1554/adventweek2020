bbb7cb6d1233d00391d16fef848c7d73de25ac9b
Полина Демакова <pademakova@edu.hse.ru>
2019-12-20 00:31:36 +0300

    add snake and tetris games

---
+++ TheBESTTetrisEVER/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace TheBESTTetrisEVER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.linesLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(482, 275);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Padding = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreLabel.Size = new System.Drawing.Size(59, 20);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "label1";
+            this.scoreLabel.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            // 
+            // linesLabel
+            // 
+            this.linesLabel.AutoSize = true;
+            this.linesLabel.Location = new System.Drawing.Point(482, 229);
+            this.linesLabel.Name = "linesLabel";
+            this.linesLabel.Size = new System.Drawing.Size(51, 20);
+            this.linesLabel.TabIndex = 1;
+            this.linesLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 744);
+            this.Controls.Add(this.linesLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label linesLabel;
+    }
+}
+
